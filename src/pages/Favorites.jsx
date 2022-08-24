1fe7973a11d00aa149c8a1da072c2958f8d4f387
ScottProuty <proutyscottl@gmail.com>
2022-08-24 12:58:54 -0700

    Add react framework and navigation bar to app

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+function FavoritesPage() {
+  return (
+    <div>
+      <h1>All Favorites Page</h1>
+    </div>
+  );
+}
+
+export default FavoritesPage;
---
+++ src/pages/NewMeetup.jsx
@@ -0,0 +1,9 @@
+function NewMeetupPage() {
+  return (
+    <div>
+      <h1>New Meetup Page</h1>
+    </div>
+  );
+}
+
+export default NewMeetupPage;
